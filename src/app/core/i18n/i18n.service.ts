8ccf025f086dcf183769fde668b27c8029fbe1fe
devcui <devcui@outlook.com>
2021-11-23 09:21:10 +0800

    fix: fix use default language when the browser language is not in the list

--- src/app/core/i18n/i18n.service.ts
+++ src/app/core/i18n/i18n.service.ts
@@ -74,11 +74,8 @@ export class I18NService extends YunzaiI18nBaseService {
     cogSrv: YunzaiConfigService
   ) {
     super(cogSrv);
-
     const defaultLang = this.getDefaultLang();
-    if (this._langs.findIndex(w => w.code === defaultLang)) {
-      this._defaultLang = defaultLang;
-    }
+    this._defaultLang = this._langs.findIndex(w => w.code === defaultLang) === -1 ? DEFAULT : defaultLang;
   }
 
   private getDefaultLang(): string {
